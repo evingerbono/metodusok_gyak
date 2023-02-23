dd375603051ce7952321757a92df1c9d29ea5782
evingerbono <bono0823@gmail.com>
2023-02-23 15:08:42 +0100

    Metódusok generálása

---
+++ src/metodusok/Metodusok.java
@@ -0,0 +1,25 @@
+
+package metodusok;
+
+public class Metodusok {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    private static int elso10szamosszege(){
+     int osszeg=0;
+        for (int i = 0; i < 10; i++) {
+            osszeg+=1;
+        }
+        return osszeg;
+    }
+    
+    private static int osszead(int a , int b){
+        return a+b;
+    }
+    
+    private static void kiir(String szoveg){
+        System.out.print(szoveg);
+    } 
+}
