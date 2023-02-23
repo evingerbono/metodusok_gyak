5d98398779b70071d30216bec11b3cfb8e74a65e
evingerbono <bono0823@gmail.com>
2023-02-23 16:32:59 +0100

    Equalizer_fájlban_színes_kiíratás_paraméterezve

--- src/metodusok/Equalizer.java
+++ src/metodusok/Equalizer.java
@@ -4,19 +4,29 @@ package metodusok;
 import java.util.Random;
 
 public class Equalizer {
+    private static Random rnd = new Random();
     public static void main(String[] args) {
-      eq();
-      eq();
-      eq();
-      eq();
-      eq();
+      equalizer5lefut();
+    }
+
+    private static void equalizer5lefut() {
+        eq();
+        eq(12);
+        eq();
+        eq(8);
+        eq();
     }
 
     private static void eq() {
-        Random rnd = new Random();
         int also=3, felso=7;
         int hossz= rnd.nextInt(felso-also+1)+also;
-        System.out.println(hossz);
+        for (int i = 0; i < hossz; i++) {
+            System.out.printf("\u001B[45m" + " ");
+        }
+        System.out.println("");
+    }
+    
+    private static void eq(int hossz) {
         for (int i = 0; i < hossz; i++) {
             System.out.printf("\u001B[45m" + " ");
         }
